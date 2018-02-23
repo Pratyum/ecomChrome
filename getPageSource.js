3a2202f09c351ee3caecf56f940254f2c99717f8
Pratyum Jagannath <pappi.51123@gmail.com>
2018-02-24 05:30:41 +0800

    Cart working on flipkart scraping all sorts of stuff

---
+++ getPageSource.js
@@ -0,0 +1,38 @@
+// @author Rob W <http://stackoverflow.com/users/938089/rob-w>
+// Demo: var serialized_html = DOMtoString(document);
+
+function DOMtoString(document_root) {
+    var html = '',node = document_root.querySelectorAll('a');
+    var endJson = {html:'',data:[]};
+    node.forEach(function (value, index){
+        if(index > 1 && index < node.length - 9 && index%2 == 0){
+            var url = value.href;
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open( "GET", url, false ); // false for synchronous request
+            xmlHttp.send( null );
+            var parser = new DOMParser();
+            var parsedHTML = parser.parseFromString(xmlHttp.response,'text/html');
+            var weight = '',dimensions='';
+            parsedHTML.querySelectorAll('.sNqDog').forEach(function(value,index){ 
+                if(value.innerText.includes('kg')){
+                    weight = value.innerText.split()[0];
+                }
+                if(value.innerText.match(/\b((\d+\.?\d* mm)( x ))+\b/i)){
+                    dimensions = value.innerText.split()[0];
+                }
+            });
+            var price = parsedHTML.getElementsByClassName('_1vC4OE _37U4_g')[0].innerText;
+            var item = {name:node[index+1].innerText,weight:weight,dimensions:dimensions,price:price};
+            endJson.data.push(item);
+            html += '<li class="list-group-item">' + value.outerHTML + node[index+1].innerText +' weight=' + weight + ' dimensions= ' + dimensions + ' price='+ price +'</li>';
+        }
+    });
+    endJson.html = html;
+    return endJson;
+}
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: DOMtoString(document),
+    orgSource: document
+});
---
+++ popup.js
@@ -0,0 +1,23 @@
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    console.log(request.source.data);
+    message.innerHTML = request.source.html;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPageSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
